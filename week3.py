3ae2108abbd9486ccda38fa7cc638d44a35029eb
jasnishrestha28@gmail.com <jasnishrestha28@gmail.com>
2026-09-24 12:51:34 +0545

    firstCV

---
+++ week3.py
@@ -0,0 +1,19 @@
+# def sum3nums(x,y,z=5):
+#     answer = x + y + z 
+#     print("The answer is ",answer)
+# first=int(input("enter x :"))
+# last= int(input("enter y :"))
+# sum3nums(x=first,y=last,z=6)
+
+# def addition(a,b,c,d):
+#     '''This function takes 4 input 8 returns the sum of those number'''
+#     return a+b+c+d
+# addition(a=1,b=3,c=4,d=5)
+
+# def fruitful3sum (a=1,b=1,c=1):
+#     answer = a+b+c
+#     return"The answer is"+str(answer)
+# print(fruitful3sum())
+
+ 
+
